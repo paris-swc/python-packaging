cd8f01f47308a09747f5e6990dc66d238dfcdc42
Erik M. Bray <erik.bray@lri.fr>
2016-04-28 15:10:25 +0200

    We can add a test() function to our package so that users can run the tests (if they have the test framework installed)

--- simcluster/__init__.py
+++ simcluster/__init__.py
@@ -1,1 +1,14 @@
 from .simcluster import *
+
+
+def test():
+    try:
+        import pytest
+    except ImportError:
+        print("py.test is required to run the test suite for simcluster")
+        return
+
+    import os
+    path = os.path.join(os.path.dirname(__file__), 'tests')
+    os.chdir(path)
+    pytest.main([os.path.relpath(path)])
