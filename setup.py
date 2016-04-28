11d818e61712e486508537c78c030d10c6635a30
Erik M. Bray <erik.bray@lri.fr>
2016-04-28 15:10:05 +0200

    If we want to install the tests we must add simcluster.tests to the packages list, and we must install the package_data.

--- setup.py
+++ setup.py
@@ -9,7 +9,8 @@ setup(
     author_email='your.name@example.com',  # so people can pester you ;)
     version='0.1',
     # Python packages to install (i.e. the directory name)
-    packages=['simcluster'],
+    packages=['simcluster', 'simcluster.tests'],
+    package_data={'simcluster.tests': ['refs/*.npy']},
     # This is the full name of the script "simcluster"; this will be installed to a bin/ directory
     scripts=[os.path.join('scripts', 'simcluster')]
 )
